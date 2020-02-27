536f549133fdefe947c64b4ea6abbffa41c5ef9a
LucasNemeth <l_orion_n@hotmail.com>
2020-02-27 12:38:58 -0500

    Updates

---
+++ precache-manifest.e050fdb597475c628ea4589d74d8b1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7529ef49728cbb0e0c368bcea470bca3",
+    "url": "/rijks-code-along/index.html"
+  },
+  {
+    "revision": "1300240615dfbdd6092c",
+    "url": "/rijks-code-along/static/css/main.05746ab6.chunk.css"
+  },
+  {
+    "revision": "87c2152f8e58bbdbf820",
+    "url": "/rijks-code-along/static/js/2.e9fb820f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rijks-code-along/static/js/2.e9fb820f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1300240615dfbdd6092c",
+    "url": "/rijks-code-along/static/js/main.8c106b1a.chunk.js"
+  },
+  {
+    "revision": "f83e7b5fbbd1485034de",
+    "url": "/rijks-code-along/static/js/runtime-main.f5adffe8.js"
+  }
+]);
